b6beaf2594f3755cf79e0534d4b36719ed5db15a
yuki hara <hara.0509@icloud.com>
2023-10-12 08:43:16 +0900

    remove:不要なメソッド削除

--- webserver.rb
+++ webserver.rb
@@ -17,32 +17,6 @@ class WebServer
     end
     server.close
   end
-
-  private
-
-    def parse_http_request(request)
-      request_line, remain = request.split("\r\n", 2)
-      request_header, request_body = remain.split("\r\n\r\n", 2)
-      method, path, http_version = request_line.split(' ')
-      [method, path, http_version, request_header, request_body]
-    end
-
-    def create_response_header(path, response_body)
-      content_type = create_content_type(path)
-
-      response_header = "#{Time.now.strftime("%a, %d %b %Y %H:%M:%S GMT") + "\r\n"}"
-      response_header += "Host: HenaServer/0.1\r\n"
-      response_header += "Content-Length: #{response_body.bytesize}\r\n"
-      response_header += "Connection: Close\r\n"
-      response_header += "Content-Type: #{content_type}\r\n"
-
-      response_header
-    end
-
-    def create_content_type(path)
-      ext = path.split('.')[-1]
-      MIME_TYPES[ext.to_sym]
-    end
 end
 
 WebServer.new.server
